25b3b37bc7c9fc37093720e9e1477d1eb83d5c89
Oliver Leisalu <balduran123@gmail.com>
2024-04-04 10:27:14 +0300

    x

--- src/select/DbSelect04Where.ts
+++ src/select/DbSelect04Where.ts
@@ -7,19 +7,37 @@ export class DbSelect04Where<Result, UsedAliases, WithAliases, Tables, UsedTable
         return new DbSelect05GroupBy(this.builder)
     }
 
-    public where<UsedTables2 extends string>(
-        col: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<UsedTables2>, Value<UsedTables2, unknown, SQL_BOOL>>
-    ): DbSelectWhere2<Result, UsedAliases, WithAliases, Tables, UsedTables, LastType, CTX> {
-        this.builder.where(col as AnyValue)
-        return new DbSelectWhere2(this.builder)
+    public where<T1 extends string>(
+        col1: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T1>, Value<T1, unknown, SQL_BOOL>>
+    ): DbSelect05GroupBy<Result, Tables, UsedTables, LastType, CTX>
+    public where<T1 extends string, T2 extends string>(
+        col1: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T1>, Value<T1, unknown, SQL_BOOL>>,
+        col2: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T1>, Value<T1, unknown, SQL_BOOL>>
+    ): DbSelect05GroupBy<Result, Tables, UsedTables, LastType, CTX>
+    public where<T1 extends string, T2 extends string, T3 extends string>(
+        col1: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T1>, Value<T1, unknown, SQL_BOOL>>,
+        col2: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T2>, Value<T2, unknown, SQL_BOOL>>,
+        col3: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<T3>, Value<T3, unknown, SQL_BOOL>>
+    ): DbSelect05GroupBy<Result, Tables, UsedTables, LastType, CTX>
+
+    public where(
+        ...cols: any[]
+    ): DbSelect05GroupBy<Result, Tables, UsedTables, LastType, CTX> {
+
+        for (let i = 0; i < cols.length; i++) {
+            this.builder.where(cols[i] as unknown as AnyValue)
+        }
+        return new DbSelect05GroupBy(this.builder)
     }
 }
 
-export class DbSelectWhere2<Result, UsedAliases, WithAliases, Tables, UsedTables, LastType, CTX> extends DbSelect05GroupBy<Result, Tables, UsedTables, LastType, CTX> {
-    public where<UsedTables2 extends string>(
-        col: CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<UsedTables2>, Value<UsedTables2, unknown, SQL_BOOL>>
-    ): DbSelectWhere2<Result, UsedAliases, WithAliases, Tables, UsedTables, LastType, CTX> {
-        this.builder.where(col as AnyValue)
-        return new DbSelectWhere2(this.builder)
-    }
-}
+// Recursive type to handle same thing, but TS does not recognize that input is a duple, but takes it as (A | B)[], but needed would be [A,B]
+// Can use something like this if it improves.
+// type CheckThatTablesAreAllAddedToTheQuery<Cols, Tables> = Cols extends [infer A, ...(infer B)]
+//     ? [
+//         A extends Value<infer UsedTables2, unknown, SQL_BOOL>
+//             ? CheckIfAliasedTablesAreReferenced<Tables, TrueRecord<UsedTables2>, Value<UsedTables2, unknown, SQL_BOOL>>
+//             : "Invalid argument",
+//         ...CheckThatTablesAreAllAddedToTheQuery<B, Tables>
+//     ]
+//     : []
