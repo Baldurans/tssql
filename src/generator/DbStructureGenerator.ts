cfaf725c62cc31a77c246d428698417c34acc721
Oliver Leisalu <balduran123@gmail.com>
2024-04-08 17:26:03 +0300

    gen

---
+++ src/generator/DbStructureGenerator.ts
@@ -0,0 +1,9 @@
+import {TableStructure} from "./MysqlTableStructureParser";
+
+export class DbStructureGenerator {
+
+    public static generate(table: TableStructure[], outDir: string) {
+        console.log(table)
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './SqlFunctions';
 export * from './SqlExpression';
 export * from './Types';
 export * from './MysqlTable';
+export * from './generator/DbStructureGenerator';
+export * from './generator/MysqlTableStructureParser';
