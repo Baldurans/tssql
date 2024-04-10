5aafa87425c38f5f5b7beb0784213d7685d1c93e
Oliver Leisalu <balduran123@gmail.com>
2024-04-10 14:44:06 +0300

    x

--- src/select/parts/S7Exec.ts
+++ src/select/parts/S7Exec.ts
@@ -14,8 +14,18 @@ export interface ExecMethods<Result> extends SqlSelectQuery<Result>, UnionMethod
 
     toString(lvl?: number): string
 
+    /**
+     * Example of an executor method.
+     * Notice that it can force existence of other arguments in the exec call. In this case forcing ctx to be added.
+     * async <Result>(sql: string, ctx: Ctx): Promise<Result> => {
+     *     return someDb.query(ctx, sql);
+     * }
+     */
     exec<Args extends any[]>(executor: SelectExecutor<Result, Args>, ...args: Args): Promise<Result[]>
 
+    /**
+     * Same as exec, but returns one row
+     */
     execOne<Args extends any[]>(executor: SelectExecutor<Result, Args>, ...args: Args): Promise<Result>
 
 }
